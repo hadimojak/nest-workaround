6ec65a559ac9cd20857f3ed6e63d831e57b4a63c
h.arbabi <hadimojak@gmail.com>
2025-05-03 10:16:29 +0330

    init

---
+++ src/main.ts
@@ -0,0 +1,22 @@
+import { Module, Controller, Get } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+
+@Controller()
+class AppController {
+  @Get()
+  getRootRoute() {
+    return "hi there";
+  }
+}
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+async function bootStarp() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000);
+}
+
+bootStarp();
